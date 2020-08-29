cf4f9b7a52cb8107f21b14dac00b1c287b1da2ff
php110 <2398586061@qq.com>
2020-08-29 16:35:49 +0800

    获取路径中的参数，规则既匹配

--- main.go
+++ main.go
@@ -14,9 +14,20 @@ func main() {
 		})
 	})
 
+	// 此规则能够匹配/user/john这种格式，但不能匹配/user/ 或 /user这种格式
 	r.GET("/user/:name", func(c *gin.Context) {
 		name := c.Param("name")
 		c.String(http.StatusOK, "user name:%s", name)
 	})
+
+	// 但是，这个规则既能匹配/user/name/格式也能匹配/user/name/action这种格式
+	// 如果没有其他路由器匹配/user/name，它将重定向到/user/name/
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
+	})
+
 	r.Run()
 }
