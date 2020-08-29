3a8765902ae2c09663f4659f147d2b3283519030
php110 <2398586061@qq.com>
2020-08-29 16:25:24 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/test", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "test",
+		})
+	})
+
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
+		c.String(http.StatusOK, "user name:%s", name)
+	})
+	r.Run()
+}
